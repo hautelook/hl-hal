63f516244fd2dc26e4d5235da0e456a66335429c
Ken Browning <ken.browning@hautelook.com>
2015-04-06 17:15:14 -0700

    change formatting

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 var UriTemplate = require('uritemplate');
 
-function isArray(value) {
-    return Object.prototype.toString.call(value) === '[object Array]';
-}
-
-function makeArray(value) {
-    return isArray(value) ? value : [value];
-}
-
-function getRelation(resource, type, relation) {
-    return (resource[type] && resource[type][relation])
-        ? makeArray(resource[type][relation])
-        : [];
-}
-
-function expandUriTemplate(template, vars) {
-    return UriTemplate.parse(template).expand(vars);
-}
-
 module.exports = {
     resources: function(resource, relation) {
         return getRelation(resource, '_embedded', relation);
@@ -29,7 +11,32 @@ module.exports = {
 
     hrefs: function(resource, relation, vars) {
         return getRelation(resource, '_links', relation).map(function(link) {
-            return link.templated ? expandUriTemplate(link.href, vars) : link.href
+            return link.templated ? expandUriTemplate(link.href, vars) : link.href;
         });
     }
 };
+
+function getRelation(resource, type, relation) {
+    if (!resource[type]) {
+        return [];
+    }
+    return makeArray(resource[type][relation]);
+}
+
+function makeArray(value) {
+    if (typeof value === 'undefined') {
+        return [];
+    }
+    if (isArray(value)) {
+        return value;
+    }
+    return [value];
+}
+
+function isArray(value) {
+    return Object.prototype.toString.call(value) === '[object Array]';
+}
+
+function expandUriTemplate(template, vars) {
+    return UriTemplate.parse(template).expand(vars);
+}
